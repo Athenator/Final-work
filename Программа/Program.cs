d2e973a683337c22fce9d76e73cfdf10f3cd55fc
Ruso <tanor98@mail.ru>
2022-08-10 20:23:58 +0500

    выполнен 1 пункт решения

---
+++ Программа/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите длинну массива: ");
+int l = int.Parse(Console.ReadLine()!);
+
+string [] array1;
+
+string [] array2;
+
+array1 = new string[l];
+array2 = new string[l];
+
+
