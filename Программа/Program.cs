01a72f258c0a72e3c89687db932730389a17471e
Ruso <tanor98@mail.ru>
2022-08-10 20:27:07 +0500

    создана функция заполнения обоих массивов, введенными значениями в строках, так же создана функция вывода первого масива

--- Программа/Program.cs
+++ Программа/Program.cs
@@ -8,4 +8,22 @@ string [] array2;
 array1 = new string[l];
 array2 = new string[l];
 
+for (int i = 0; i < l; i++)
+{
+  Console.Write($"Введите {i+1} элемент массива: ");
+  array1 [i] = Console.ReadLine()!;
+  array2 [i] = array1 [i];
+  
+}
+
+Console.Write("Вывод массива: ");
+Console.Write(" [");
+for (int i = 0; i < l; i++)
+{
+  Console.Write(" ");
+  Console.Write(array1[i]);
+  Console.Write(" ");
+
+}
+Console.Write("] -> ");
 
